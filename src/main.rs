a184a09e9e74162878388fadfacd8ccee7833941
icecloud12 <johnsonjaurigue2@gmail.com>
2024-03-19 22:24:38 +0800

    feat: use std::env

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod network;
 async fn main() {
     dotenv().ok();
     let router = router().await;
-    let address = "192.168.3.8";
-    let port = 3000;
+    let address = std::env::var("ADDRESS").unwrap();
+    let port = std::env::var("PORT").unwrap();
     let listener = tokio::net::TcpListener::bind(format!("{address}:{port}",address = &address, port = &port)).await.unwrap();
     println!("now listening in {}:{}",address,port);
     axum::serve(listener, router).await.unwrap();    
